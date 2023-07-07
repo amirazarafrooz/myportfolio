b4349e4f301ae3dd17316231c9e809f7c305151a
amirhossein azarafrooz <amirhosseinazarafrooz@gmail.com>
2023-07-07 23:17:37 +0330

    fix: modify card from projectsection

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -5,6 +5,7 @@ import { Cardproject, Button } from '../components';
 export const ProjectsSection = () => {
     return (
         <div className='projectssection__container'>
+            <h2>Projects</h2>
             <Cardproject />
             <Cardproject />
             <Button value="SEE ALL PROJECTS" cssClass="wide__btn" url="/projectspage"></Button>
