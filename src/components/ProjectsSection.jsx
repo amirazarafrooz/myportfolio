8f5278ba9658ec016c269232aad4bfb4717d93f7
amirhossein azarafrooz <amirhosseinazarafrooz@gmail.com>
2023-07-10 17:06:09 +0330

    make project section responsive

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -6,11 +6,12 @@ import { myprojects } from '../data/Project-data';
 export const ProjectsSection = () => {
     return (
         <div className='projectssection__container'>
+<div className='projrct__card'>
             <h2>Projects</h2>
-
             <div className="productList">
           {myprojects.filter(({id}) => id<2)
           .map(({title,imgLink,tags,caption}) => <Cardproject title={title} tags={tags} caption={caption} imgLink={imgLink}/>)}
+        </div>
         </div>
             <Button value="SEE ALL PROJECTS" cssClass="wide__btn" url="/Projects"></Button> 
         </div>
