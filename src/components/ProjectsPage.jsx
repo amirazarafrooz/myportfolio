d3a7837d99582bd4210ff44fe3d14222273e987c
amirhossein azarafrooz <amirhosseinazarafrooz@gmail.com>
2023-07-10 14:09:40 +0330

    make project page responsive

--- src/components/ProjectsPage.jsx
+++ src/components/ProjectsPage.jsx
@@ -4,10 +4,12 @@ import { myprojects } from '../data/Project-data';
 
 export const ProjectsPage = () => {
     return (
+        <div className='progectpage'>
         <div className="projectpage__container">
             <h2>Projects</h2>
         <div className='projectpage__cards'>
            {myprojects.map(({title,imgLink,tags,caption}) => <Cardproject title={title} tags={tags} caption={caption} imgLink={imgLink}/>)}
+        </div>
         </div>
         </div>
     );
