c0162a0d9f1c11ec8a1e279572074997eb75c40d
amirhossein azarafrooz <amirhosseinazarafrooz@gmail.com>
2023-07-13 23:11:14 +0330

    fix: package.json and main

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import {BrowserRouter, HashRouter} from "react-router-dom";
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <HashRouter basename={process.env.PUBLIC_URL}>
+  <BrowserRouter>
   <App />
-</HashRouter>,
+</BrowserRouter>,
 )
